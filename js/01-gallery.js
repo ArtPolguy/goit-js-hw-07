3740376543deb1cfff7a4ca9ada5ef1214752407
Artur Polguy <art.polguy@gmail.com>
2022-11-11 16:13:02 +0200

    update_js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryContainer.addEventListener(`click`, onImgGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join(" ");
+}
+
+function onImgGalleryContainerClick(evt) {
+  console.log(evt.target);
+}
